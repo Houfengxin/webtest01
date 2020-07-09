0a381b7b8a22cfad2770eb29c3eb981bf3a4f248
Houfengxin <18192062162@163.com>
2020-07-09 18:29:15 +0800

    second commit

---
+++ src/main/java/com/example/webpratice/WebpraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.webpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebpraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webpratice/WebpraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
